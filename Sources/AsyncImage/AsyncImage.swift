fcbd0fadbf5fd87d1084dd6799f093354f6d191c
yangmengjiao <mengjiaoshun@gmail.com>
2021-08-17 15:57:35 -0500

    add asyn image

--- Sources/AsyncImage/AsyncImage.swift
+++ Sources/AsyncImage/AsyncImage.swift
@@ -1,3 +1,56 @@
-struct AsyncImage {
-    var text = "Hello, World!"
+
+import SwiftUI
+import Combine
+
+@available(iOS 14, *)
+
+/// This is a view for download image from url,
+public struct AsyncImageView: View {
+    @ObservedObject private var downloader: ImageDownloader
+    
+    private var image: some View {
+        Group {
+            if downloader.image != nil {
+                Image(uiImage: downloader.image!).resizable().scaledToFit()
+            } else {
+                ProgressView()
+            }
+        }
+    }
+    
+    init(url: URL) {
+        downloader = ImageDownloader(url: url)
+    }
+    public var body: some View {
+        image.onAppear {
+            downloader.start()
+        }
+    }
+}
+
+class ImageDownloader: ObservableObject {
+    @Published private(set) var image: UIImage?
+    private let url: URL
+    private var cancellable: AnyCancellable?
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func start() {
+        cancellable = URLSession(configuration: .default)
+            .dataTaskPublisher(for: url)
+            .map {UIImage(data: $0.data)}
+            .replaceError(with: UIImage(systemName: "xmark.circle"))
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.image, on: self)
+    }
+    
+    func stop()  {
+        cancellable?.cancel()
+    }
+    
+    deinit {
+        stop()
+    }
 }
